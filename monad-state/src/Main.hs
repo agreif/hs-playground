bf825fd9c08b1c2b417dfa895c51bb5639ef5163
Alex Greif <alex.greif@gmail.com>
2022-12-29 13:10:40 +0100

    minor

--- monad-state/src/Main.hs
+++ monad-state/src/Main.hs
@@ -3,3 +3,50 @@ module Main (main) where
 main :: IO ()
 main = do
   putStrLn "hello world"
+
+type ProgState = Int
+
+foo :: StTr ()
+foo = do
+  let a = runStTr (bar 1) 1
+  return ()
+
+bar :: Int -> StTr String
+bar i = do
+  pure ""
+
+--
+
+-- State Transformer
+newtype StTr a = StTr {runStTr :: ProgState -> (a, ProgState)}
+
+instance Functor StTr where
+  -- fmap :: (a -> b) -> ST a -> ST a
+  fmap f stx =
+    StTr
+      ( \s ->
+          let (x, s') = runStTr stx s
+           in (f x, s')
+      )
+
+instance Applicative StTr where
+  -- pure :: a -> ST a
+  pure x = StTr (\s -> (x, s))
+
+  -- (<*>) :: ST (a -> b) -> ST a -> ST b
+  stf <*> stx =
+    StTr
+      ( \s ->
+          let (f, s') = runStTr stf s
+              (x, s'') = runStTr stx s'
+           in (f x, s'')
+      )
+
+instance Monad StTr where
+  -- (>>=) :: ST a -> (a -> ST b) -> ST b
+  stx >>= f =
+    StTr
+      ( \s ->
+          let (x, s') = runStTr stx s
+           in runStTr (f x) s'
+      )
